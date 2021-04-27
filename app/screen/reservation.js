80c26bd8aac72219e007b9cc4a71e3428eae7cf2
naaoufal <naoufelbenmensour@gmail.com>
2021-04-27 14:15:16 +0000

    add files

--- app/screen/reservation.js
+++ app/screen/reservation.js
@@ -5,6 +5,17 @@ import Button from '../shared/Button'
 import {useHistory} from 'react-router-native'
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
+// import nodemailer from 'nodemailer'
+
+// nodemailer configuration : 
+// var transport = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "e2e5f4e84f538b",
+//       pass: "7ed440c35117fb"
+//     }
+// });
 
 // firebase configuration :
 var firebaseConfig = {
@@ -37,6 +48,21 @@ export default function Reservation () {
             telephone : telephone,
             email : email
         }).then((res) => {
+        // exports.emailSender = functions.https.onRequest((req, res) => {
+        //     const mailOptions = {
+        //     from: 'mare32980@gmail.com', //Adding sender's email
+        //     to: email, //Getting recipient's email by query string
+        //     subject: 'Email Sent via Firebase', //Email subject
+        //     html: '<b>Sending emails with Firebase is easy!</b>' //Email content in HTML
+        // };
+    
+        // return transporter.sendMail(mailOptions, (err, info) => {
+        //     if(err){
+        //         return res.send(err.toString());
+        //     }
+        //     return res.send('Email sent successfully');
+        // });
+    //});
             setNom(""),
             setPrenom(""),
             setTelephone(""),
