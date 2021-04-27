6e1b83d1247cc0cf5e59ed73e92cbe535b779f4b
abdelhamid1A <hamoda.abdo@outlook.fr>
2021-04-27 15:37:41 +0000

    calc

--- app/screen/Credit.js
+++ app/screen/Credit.js
@@ -1,13 +1,68 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import {useHistory} from 'react-router-native'
 import Button from '../shared/Button'
+import Slider from "react-native-sliders";
+import {styles} from '../style/styles'
 
 export default function Login() {
+    const [mt,setMt] = useState(4000)
+    const [mois,setMois] = useState(12)
+    const [mensu,setMensu] = useState(2463)
+    const [interetGlobale,setInteretGlobale] = useState(0)
+    const [interetParMois,setInteretParMois] = useState(0)
+    // const [mensu,setMensu] = useState(0)
     const history = useHistory()
+    function calcAmortissment(mt,mois){
+        mois = mois.value
+        mt = mt.value
+        // console.log(mt);
+        // console.log(mois);
+        const a = parseFloat(mt/mois)
+        // console.log(a);
+        // console.log(typeof(a));
+        const frais = parseFloat(((0.71*mois)*50)/mois)
+        var interGloble = parseFloat((mt*mois*20)/1200)
+        setInteretGlobale(interGloble)
+        var iParMois = parseFloat((interGloble/mois).toFixed(2))
+        setInteretParMois(iParMois)
+        var Amortissement = parseFloat((a + iParMois + frais).toFixed(2))
+        setMensu(Amortissement)
+    }
+    // console.log(parseFloat(mensu));
+    // console.log(typeof(parseFloat(mensu)));
+
     return (
-        <View>
-            <Text>Login page</Text>
+        <View >
+            <Text>Mt</Text>
+            <Slider
+               minimumValue={4000}
+               maximumValue={500000}
+               onValueChange={value => {
+                //    console.log(value);
+                    value = parseFloat(value[0].toFixed(0))
+                    setMt({ value })
+                    calcAmortissment(mt,mois)
+                }}
+            />
+            
+            <Text>{mt.value}</Text>
+            <Text>mois</Text>
+            <Slider
+               minimumValue={12}
+               maximumValue={84}
+               onValueChange={value => {
+                    value = parseInt(value[0].toFixed(0))
+                    setMois({ value })
+                    calcAmortissment(mt,mois)     
+                }}
+            />
+            <Text>{mois.value}</Text>
+            <Text>Mensualité</Text>
+          
+            <Text>{mensu ? mensu : ''}</Text>
+            <Text>intert globale :{interetGlobale ? interetGlobale : ''}</Text>
+            <Text>interet par mois : {interetParMois ? interetParMois : ''}</Text>
             <Button title='reservation' onPress={() => {
                 history.push("/reservation");
             }} />
--- app/screen/reservation.js
+++ app/screen/reservation.js
@@ -7,19 +7,13 @@ import {useHistory} from 'react-router-native'
 export default function Reservation () {
     const history = useHistory()
     return (
-<<<<<<< HEAD
-        <TextInput
-        style={styles.inp}
-        placeholder={'Enter Votre Nom'}
-        />
-=======
+        
         <View style={styles.cont}>
             <TextInput
             style={styles.inp}
             placeholder={'Enter Votre Nom'}
             />
         </View>
->>>>>>> e70a05e3d615a5cfcd07d6eb7e85c9e7ecec3b04
     )
 }
 
